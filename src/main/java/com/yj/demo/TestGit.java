3b71b49891a8e244d6ec38e867dd4af2aa87e9cc
zhumo1998 <2546747893@qq.com>
2020-06-09 11:37:11 +0800

    第二次使用git提交项目

--- src/main/java/com/yj/demo/TestGit.java
+++ src/main/java/com/yj/demo/TestGit.java
@@ -2,8 +2,10 @@ package com.yj.demo;
 
 public class TestGit {
     public void go(){
+        System.out.println("11");
         System.out.println("22");
         System.out.println("修改过了");
+        System.out.println("第二次修改");
     }
 
 }
